c142781bc4f72e029b2282865713ced3160c4c13
Aamal Hussain <aamal.hussain@physicsx.ai>
2025-07-04 06:29:57 +0100

    create shapenet car

--- scripts/make_dataset.py
+++ scripts/make_dataset.py
@@ -2,8 +2,9 @@ import os
 import h5py
 import numpy as np
 import pyvista as pv
+import pymeshfix as mf
 from pathlib import Path
-from concurrent.futures import ProcessPoolExecutor, as_completed
+from concurrent.futures import ThreadPoolExecutor, as_completed
 
 from tqdm import tqdm
 
@@ -11,12 +12,21 @@ _BASE_PATH = Path("/mnt/storage01/workspace/research/gen11/shapenet_13")
 _H5_Path = _BASE_PATH / "car_dataset"
 
 
-def process_files(file_path: Path):
-    print(f"Processing files in: {file_path}")
-    files = file_path.rglob("*.obj")
-    for pth in files:
+def process_files(pth: Path):
+    try:
         name = pth.parent.stem
         mesh = pv.read(pth)
+
+        if not mesh.is_manifold:
+            meshfix = mf.MeshFix(mesh)
+            meshfix.repair(verbose=False)
+            fixed_mesh = meshfix.mesh
+            if fixed_mesh.is_manifold:
+                mesh = fixed_mesh
+            else:
+                raise ValueError("Skipping this mesh as it is not manifold")
+
+
         mesh = mesh.compute_cell_sizes(length=False, area=True, volume=False)
         mesh = mesh.compute_normals(cell_normals=True, point_normals=False)
         mesh_data = {
@@ -35,20 +45,28 @@ def process_files(file_path: Path):
         with h5py.File(_H5_Path / split / f"{name}.h5", "w") as f:
             for key, value in mesh_data.items():
                 f.create_dataset(key, data=value)
-        f.close()
-    print(f"Finished processing files in: {file_path}")
 
+    except Exception as e:
+        raise ValueError(f"Error processing file {pth}: {e}") from e
+
+    return
 
 if __name__ == "__main__":
-    categories = list(_BASE_PATH.glob("*/"))
 
     os.makedirs(_H5_Path, exist_ok=True)
     os.makedirs(_H5_Path / "train", exist_ok=True)
     os.makedirs(_H5_Path / "test", exist_ok=True)
 
-    with ProcessPoolExecutor() as exec:
-        futures = exec.map(process_files, categories)
-        for future in tqdm(as_completed(futures), total=len(categories)):
-            pass
+    _CAR_PATH = _BASE_PATH / "02958343"
+    files = list(_CAR_PATH.rglob("*.obj"))
+
+    with ThreadPoolExecutor() as exec:
+        futures = {exec.submit(process_files, f): f for f in files}
+        for future in tqdm(as_completed(futures), total=len(futures)):
+            file = futures[future]
+            try:
+                future.result()
+            except Exception as e:
+                print(f"FAILED processing {file}: {e}")
 
     print("All files processed and saved to HDF5 format.")
